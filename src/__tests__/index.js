19d08f0fd854c845bf0d1b4b694f81eed2715614
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-14 19:43:51 +0100

    made a folder for testing that has to have a specific name to do tests & did one test to know how to do them

---
+++ src/__tests__/index.js
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "../App";
+
+// this is when its synchronous, but now we are rendering the homepage only when needed (lazy load). So whe need to change the test
+// test("renders without crashing", () => {
+//   render(<App />);
+//   const title = screen.getByText(/Last Search/i);
+//   expect(title).toBeInTheDocument();
+// });
+
+// test("renders without crashing", async () => {
+//   render(<App />);
+//   const title = await screen.findByText(/Last Search/i);
+//   expect(title).toBeInTheDocument();
+// });
+
+test("search form could be used", async () => {
+  render(<App />);
+  const input = await screen.findByRole("textbox");
+  const button = await screen.findByRole("button");
+
+  fireEvent.change(input, { target: { value: "Matrix" } });
+  fireEvent.click(button);
+
+  const title = await screen.findByText("Matrix");
+  expect(title).toBeVisible();
+});
