ddb382afa64cc4a13e7becc79f68652de3177000
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-14 19:37:22 +0100

    imported: logo, link & route from wouter, lazy to import when needed & suspense tot have a fallback untill its done rendering, pages and context. Then added every component to the aapp.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logo.png";
+import { Link, Route } from "wouter";
+// import Home from "./pages/Home";
+import { lazy, Suspense } from "react";
+import SearchResults from "./pages/SearchResults";
+import StaticContext from "./context/StaticContext";
+import Detail from "./pages/Detail";
+import { GifsContextProvider } from "./context/GifsContext";
+
+const HomePage = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StaticContext.Provider value={{ name: "pablo", subscribe: true }}>
+      <div className="App">
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Link to="/">
+              <img className="App-logo" src={logo} alt="Giphy logo" />
+            </Link>
+            <GifsContextProvider>
+              <Route path="/" component={HomePage} />
+              <Route path="/gif/:id" component={Detail} />
+              <Route path="/search/:keyword" component={SearchResults} />
+              <Route path="/404" component={() => <h1>404 ERROR </h1>} />
+            </GifsContextProvider>
+          </section>
+        </Suspense>
+      </div>
+    </StaticContext.Provider>
   );
 }
 
