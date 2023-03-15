286cfc20f05afa63d3b5972183a7d6fcbced46bc
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-15 02:23:33 +0100

    Commented the previous logic that was passed before as prop from the searchForm component but not anymore

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,29 @@
-import { useLocation } from "wouter";
+// import { useLocation } from "wouter";
 import useGifs from "../../hooks/useGifs";
 import ListOfGifs from "../../component/ListOfGifs/ListOfGifs";
 import TrendingSearches from "../../component/TrendingSearches";
 import "./style.css";
 import SearchForm from "component/SearchForm";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 // import { useMemo } from "react";
 import { Helmet } from "react-helmet";
 
 // const POPULAR_GIFS = ["Colombia", "Venezuela", "Argentina", "Ecuador", "Spain"];
 
 const Home = () => {
-  const [path, pushLocation] = useLocation();
+  // This logic is done now in the searchForm, because we i'm not passing any props
+  // const [path, pushLocation] = useLocation();
 
   const { gifs } = useGifs();
 
   // The useCallback will save this function untill the dependancy(pushLocation) of this function changes. If this changes it will create the function again
-  const handleSubmit = useCallback(
-    ({ keyword }) => {
-      pushLocation(`/search/${keyword}`);
-    },
-    [pushLocation]
-  );
+  // This function can be use if searchForm passes the prop onSubmit with keyword information and then retreaved back here in this function to make it work
+  // const handleSubmitSearchForm = useCallback(
+  //   ({ keyword }) => {
+  //     pushLocation(`/search/${keyword}`);
+  //   },
+  //   [pushLocation]
+  // );
 
   // SearchForm rerenders all the time. You can use a useMemo hook to save the value and return it in the homepage (Not so good practice this way)
   // const element = useMemo(
@@ -40,7 +42,7 @@ const Home = () => {
       </Helmet>
       <div className="App-home">
         {/* {element} */}
-        <SearchForm onSubmit={handleSubmit} />
+        <SearchForm />
         <div className="App-results">
           <h3 className="App-title">Last Search</h3>
           <ListOfGifs gifs={gifs} />
