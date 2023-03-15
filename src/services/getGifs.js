074a8104074593bbea82987e972ce579177b6cd8
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-15 13:14:27 +0100

    added the rating variable to the URL

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,9 +1,14 @@
 import { API_KEY, API_URL } from "./settings";
 
-const getGifs = ({ limit = 5, keyword = "morty", page = 0 } = {}) => {
+const getGifs = ({
+  limit = 5,
+  keyword = "morty",
+  page = 0,
+  rating = "g",
+} = {}) => {
   const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${
     page * limit
-  }&rating=g&lang=en`;
+  }&rating=${rating}&lang=en`;
   return fetch(apiURL)
     .then((res) => res.json())
     .then((response) => {
