0edcbea9457ceab7260e4cfa091acaa3b15817cd
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-15 15:40:35 +0100

    Changed the SearchForm component to be able tu use useReducer

--- src/component/SearchForm/index.js
+++ src/component/SearchForm/index.js
@@ -1,12 +1,63 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { useLocation } from "wouter";
 
 const RATINGS = ["g", "pg", "pg-13", "r"];
 
+const ACTIONS = {
+  UPDATE_KEYWORD: "update_keyword",
+  UPDATE_RATING: "update_rating",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.UPDATE_KEYWORD:
+      return {
+        ...state,
+        keyword: action.payload,
+        times: state.times + 1,
+      };
+    case ACTIONS.UPDATE_RATING:
+      return {
+        ...state,
+        rating: action.payload,
+        times: state.times + 1,
+      };
+
+    default:
+      return state;
+  }
+
+  /*With if & if else
+  // if (action.type === ACTIONS.UPDATE_KEYWORD) {
+  //   return {
+  //     ...state,
+  //     keyword: action.payload,
+  //     times: state.times + 1,
+  //   };
+  // } else if (action.type === ACTIONS.UPDATE_RATING) {
+  //   return {
+  //     ...state,
+  //     rating: action.payload,
+  //     times: state.times + 1,
+  //   };
+*/
+};
+
 const SearchForm = ({ initialKeyword = "", initialRating = "g" }) => {
-  const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword));
+  // states without using useReducer
+  // const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword));
+  // const [times, setTimes] = useState(0);
+  // const [rating, setRating] = useState(initialRating);
+
+  const [state, dispatch] = useReducer(reducer, {
+    keyword: decodeURIComponent(initialKeyword),
+    rating: initialRating,
+    times: 0,
+  });
+
+  const { keyword, times, rating } = state;
+
   const [path, pushLocation] = useLocation();
-  const [rating, setRating] = useState(initialRating);
 
   const handelSubmit = (event) => {
     event.preventDefault();
@@ -14,18 +65,30 @@ const SearchForm = ({ initialKeyword = "", initialRating = "g" }) => {
     // onSubmit({ keyword });
   };
 
-  const handleInput = (event) => {
-    setKeyword(event.target.value);
+  const handleChange = (event) => {
+    // using dispatch this way if you have only one dispatch in useReducer
+    // dispatch(event.target.value);
+
+    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: event.target.value });
   };
 
   const handleChangeRating = (event) => {
-    setRating(event.target.value);
+    dispatch({ type: ACTIONS.UPDATE_RATING, payload: event.target.value });
   };
+
+  // change handlers without using useReducer
+  // const handleChange = (event) => {
+  //   setKeyword(event.target.value)
+  // }
+
+  // const handleChangeRating = (event) => {
+  //   setRating(event.target.value);
+  // };
 
   return (
     <form onSubmit={handelSubmit}>
       <input
-        onChange={handleInput}
+        onChange={handleChange}
         type="text"
         value={keyword}
         placeholder="Search a gif here..."
@@ -37,6 +100,7 @@ const SearchForm = ({ initialKeyword = "", initialRating = "g" }) => {
           <option key={rating}>{rating}</option>
         ))}
       </select>
+      <small>{times}</small>
     </form>
   );
 };
