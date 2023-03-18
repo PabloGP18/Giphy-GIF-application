de78e2ef2053f3d7dae2aed957a3d481383ca7c9
PabloGP18 <garciaplaza.pablo@gmail.com>
2023-03-18 02:39:06 +0100

    Imported switch from wouter to not have the errorpage on everypage and only on error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import logo from "./images/logo.png";
-import { Link, Route } from "wouter";
+import { Link, Route, Switch } from "wouter";
 // import Home from "./pages/Home";
 import { lazy, Suspense } from "react";
 import SearchResults from "./pages/SearchResults";
 import StaticContext from "./context/StaticContext";
 import Detail from "./pages/Detail";
 import { GifsContextProvider } from "./context/GifsContext";
+import ErrorPage from "pages/Error";
 
 const HomePage = lazy(() => import("./pages/Home"));
 
 //* using : (example :id) in path means its dynamical, using ? means its optional (example rating?)
+
 function App() {
   return (
     <StaticContext.Provider value={{ name: "pablo", subscribe: true }}>
@@ -21,13 +23,15 @@ function App() {
               <img className="App-logo" src={logo} alt="Giphy logo" />
             </Link>
             <GifsContextProvider>
-              <Route path="/" component={HomePage} />
-              <Route path="/gif/:id" component={Detail} />
-              <Route
-                path="/search/:keyword/:rating?/:language?"
-                component={SearchResults}
-              />
-              <Route path="/404" component={() => <h1>404 ERROR </h1>} />
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route
+                  path="/search/:keyword/:rating?/:language?"
+                  component={SearchResults}
+                />
+                <Route path="/gif/:id" component={Detail} />
+                <Route path="/:rest*" component={ErrorPage} />
+              </Switch>
             </GifsContextProvider>
           </section>
         </Suspense>
