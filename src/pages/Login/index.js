59fa440a9eeed5390c32a8eb46ba41ef0e88fa61
Pablo GP <garciaplaza.pablo@gmail.com>
2023-05-10 16:57:58 +0200

    2 pages to register & login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from 'component/Login'
+
+export default function loginPage() {
+  return (
+    <>
+      <h2>Login</h2>
+      <Login />
+    </>
+  )
+}
---
+++ src/pages/Register/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Register from 'component/Register/indexWithFormk'
+
+export default function RegisterPage() {
+  return (
+    <>
+      <h2>Register</h2>
+      <Register />
+    </>
+  )
+}
