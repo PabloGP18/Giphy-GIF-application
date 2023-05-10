acee60e08f6133d87c363ba7e08490de51fa5beb
Pablo GP <garciaplaza.pablo@gmail.com>
2023-05-10 16:51:40 +0200

    Added header with login and register, css todo

---
+++ src/component/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, useRoute } from 'wouter'
+
+import useUser from 'hooks/useUser'
+
+import './index.css'
+
+export default function Header() {
+  // const isLogged = false
+  const { isLogged, logout } = useUser()
+  const [match] = useRoute('/login')
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    logout()
+  }
+
+  const renderLoginButtons = ({ isLogged }) => {
+    return isLogged ? (
+      <button onClick={handleClick}>Logout</button>
+    ) : (
+      <>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </>
+    )
+  }
+
+  const content = match ? null : renderLoginButtons({ isLogged })
+
+  return <header className="gf-header">{content}</header>
+}
